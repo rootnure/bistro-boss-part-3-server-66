f0e0b6b97e35968a72cc302cf59d17ca59adbdf1
rootnure <nur.diu.2791@gmail.com>
2023-11-18 18:46:15 +0600

    README.md file updated with todays work

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bistro Boss (Part 3)
+
+## Work Continue From [This Repository](https://github.com/rootnure/bistro-boss-part-2-server-65)
+
+## Today's Work
+- Get all data from menu
+- Get all data from reviews
--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-// console.log(process.env.TOKEN_SECRET);
-
 // middleware
 app.use(cors());
 app.use(express.json());
